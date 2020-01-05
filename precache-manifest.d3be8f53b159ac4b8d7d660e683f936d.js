11c8df4b962590b27d2fb20f83e6533d8348b2fd
ellieanndesign <ellieannslinger@gmail.com>
2020-01-05 12:19:22 -0500

    Updates

---
+++ precache-manifest.d3be8f53b159ac4b8d7d660e683f936d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbc2d77aac0a4eabc91c2f2b5929cd6",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "df870ebb9b134518e782",
+    "url": "/shopping-list/static/css/main.f95b0870.chunk.css"
+  },
+  {
+    "revision": "e6805e9fed2bab9d4a0c",
+    "url": "/shopping-list/static/js/2.80feab42.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/shopping-list/static/js/2.80feab42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df870ebb9b134518e782",
+    "url": "/shopping-list/static/js/main.8cef57df.chunk.js"
+  },
+  {
+    "revision": "e41df7b1ab2cb487b8ff",
+    "url": "/shopping-list/static/js/runtime-main.02180216.js"
+  },
+  {
+    "revision": "5865ebd5c3a548469bbba8829eab6b7c",
+    "url": "/shopping-list/static/media/background.5865ebd5.jpg"
+  }
+]);
